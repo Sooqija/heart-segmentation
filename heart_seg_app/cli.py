d6b1ca049f875ce0f16db5ea7c029d60c70a3272
Cayetana <OlyninAlex@gmail.com>
2025-02-12 18:43:10 +0300

    heart-seg-app: train script improvement + model load & save + output_dir handle update train loop: + better printing mean dice, loss and etc.

--- heart_seg_app/cli.py
+++ heart_seg_app/cli.py
@@ -5,15 +5,23 @@ def main():
     parser = argparse.ArgumentParser(description="Heart Segmentation App")
     parser.add_argument("mode", choices=["train", "eval"], help="Mode: train or eval")
     parser.add_argument('--model', type=str, choices=["unet3d", "unetr"], help="Model type", required=True)
-    parser.add_argument('--image_dir', type=str, help="Path to image data", required=True)
-    parser.add_argument('--label_dir', type=str, help="Path to label data")
+    parser.add_argument('-img', '--image_dir', type=str, help="Path to image data", required=True)
+    parser.add_argument('-lbl', '--label_dir', type=str, help="Path to label data")
     parser.add_argument('--checkpoint', type=str, help="Path to model weights", default=None)
+    parser.add_argument('--output_dir', type=str, help="Directory to save model checkpoints and training statistics.")
+    parser.add_argument('-t', '--tag', type=str, help="Tag to label and track the current training run.", required=True)
+    parser.add_argument('--epochs', type=int, help="Numbers of epochs")
     args = parser.parse_args()
 
     if args.mode == "train":
         print("Training mode")
-        # print(f"Using checkpoint: {args.checkpoint}")
-        train(model=args.model, image_dir=args.image_dir, label_dir=args.label_dir, checkpoint=args.checkpoint)
+        train(model=args.model,
+              image_dir=args.image_dir,
+              label_dir=args.label_dir,
+              checkpoint=args.checkpoint,
+              output_dir=args.output_dir,
+              tag=args.tag,
+              epochs=args.epochs)
     elif args.mode == "eval":
         print("Evaluation mode")
 
--- heart_seg_app/train.py
+++ heart_seg_app/train.py
@@ -9,6 +9,8 @@ from heart_seg_app.utils.dataset import Dataset, ImagePreprocessing, LabelPrepro
 from heart_seg_app.utils.metrics import Metrics
 from heart_seg_app.models.unetr import unetr
 
+import os
+import numpy as np
 from tqdm import tqdm
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -24,7 +26,7 @@ label_color_map = {
 }; label_values = [value[0] for value in label_color_map.values()]
 
 
-def train(model, image_dir, label_dir, checkpoint=None, epochs=3):
+def train(model, image_dir, label_dir, tag, checkpoint=None, output_dir=None, epochs=3):
     
     train_dataset = Dataset(
         image_dir=image_dir,
@@ -41,8 +43,8 @@ def train(model, image_dir, label_dir, checkpoint=None, epochs=3):
     
     print("Dataset size: {}".format(len(train_dataset)))
     
-    train_size = int(0.7 * len(train_dataset))
-    val_size = int(0.15 * len(train_dataset))
+    train_size = int(0.75 * len(train_dataset))
+    val_size = int(0.2 * len(train_dataset))
     test_size = len(train_dataset) - train_size - val_size
     print(f"train_images: {train_size}, validation_images: {val_size}, test_images {test_size}")
 
@@ -55,48 +57,79 @@ def train(model, image_dir, label_dir, checkpoint=None, epochs=3):
     val_dataloader = DataLoader(val_dataset, batch_size=1)
     test_dataloader = DataLoader(test_dataset, batch_size=1)
     
+    print("Test Data:")
+    for i in test_dataset.indices:
+        print(os.path.basename(test_dataset.dataset.inputs_paths[i]))
+    
+    # model
+    
     if model == "unetr":
         model = unetr()
+    if checkpoint:
+        print("Load Checkpoint:", os.path.basename(checkpoint))
+        model.load_state_dict(torch.load(os.path.join(checkpoint), weights_only=True))
     model = model.to(device)
     
     loss_function = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
     
+    if output_dir:
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+            print("Save run's path: ", os.path.abspath(output_dir))
+        
+        checkpoints_dir = os.path.join(output_dir, "checkpoints")
+        if not os.path.exists(checkpoints_dir):
+            os.makedirs(checkpoints_dir)
+        
     # train
     for epoch in range(epochs):
         model.train()
-        mean_loss = 0
-        mean_dice = 0
+        train_loss = 0
+        train_mean_dice = 0
+        train_mean_dice_by_classes = np.zeros(len(label_values))
         for batch in tqdm(train_dataloader):
             x, y = batch[0].cuda(), batch[1].cuda()
             outputs = model(x)
             loss = loss_function(outputs, y)
             loss.backward()
-            mean_loss += loss.item()
+            train_loss += loss.item()
             optimizer.step()
             optimizer.zero_grad()
             outputs = label_postprocessing(outputs)
             targets = y.int().squeeze(0).to("cpu")
+            
             metrics = Metrics(outputs.cpu(), targets.cpu())
-            dice = metrics.meanDice().numpy()
-            mean_dice += dice
-        mean_loss /= len(train_dataloader)
-        mean_dice /= len(train_dataloader)
-        # torch.save(model.state_dict(), os.path.join("./checkpoints", "best_metric_model.pth"))
-        print(f"{epoch}: loss={mean_loss:.5}, dice={mean_dice:.5}")
+            mean_dice = metrics.meanDice().numpy()
+            dice_by_classes = metrics.Dice().numpy()
+            train_mean_dice += mean_dice
+            train_mean_dice_by_classes += dice_by_classes
+            
+        train_loss /= len(train_dataloader)
+        train_mean_dice /= len(train_dataloader)
+        train_mean_dice_by_classes /= len(train_dataloader)
+        if output_dir:
+            torch.save(model.state_dict(), os.path.join(output_dir, "checkpoints", f"{tag}.pth"))
         
-        # # validation
-        # with torch.no_grad():
-        #     model.eval()
-        #     for inputs, targets in tqdm(val_dataloader):
-        #         inputs, targets = inputs.to(device), targets.to(device)
-        #         outputs = model(inputs)
-        #         outputs = label_postprocessing(outputs)
-        #         targets = targets.int().squeeze(0).to("cpu")
-        #         metrics = Metrics(outputs, targets)
-        #         dice = metrics.Dice().numpy()
-        #         print(f"Dice: {dice}")
-        #         mean_dice = metrics.meanDice().numpy()
-        #         print(f"meanDice: {mean_dice}")
-        #     print(f"val_loss: {0}, val_dice: {mean_dice}")
+        # validation
+        with torch.no_grad():
+            model.eval()
+            val_mean_dice_by_classes = np.zeros(len(label_values))
+            val_mean_dice = 0
+            for inputs, targets in tqdm(val_dataloader):
+                inputs, targets = inputs.to(device), targets.to(device)
+                outputs = model(inputs)
+                outputs = label_postprocessing(outputs)
+                targets = targets.int().squeeze(0).to("cpu")
                 
+                metrics = Metrics(outputs, targets)
+                mean_dice = metrics.meanDice().numpy()
+                dice_by_classes = metrics.Dice().numpy()
+                val_mean_dice += mean_dice
+                val_mean_dice_by_classes += dice_by_classes
+            val_mean_dice /= len(val_dataloader)
+            val_mean_dice_by_classes /= len(val_dataloader)
+            print(f"{epoch+1}/{epochs}: train_loss={train_loss:.5}, train_mean_dice={train_mean_dice:.5}",
+                  "by classes: ", [f"{elem:.5}" for elem in train_mean_dice_by_classes])
+            print(f"{epoch+1}/{epochs}: val_mean_dice={val_mean_dice:.5}",
+                  "by classes: ", [f"{elem:.5}" for elem in val_mean_dice_by_classes])
