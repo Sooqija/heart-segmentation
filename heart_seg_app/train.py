37f3ca3919ed560a7be1c551b21575ec13d2acff
Cayetana <OlyninAlex@gmail.com>
2025-04-17 19:41:59 +0300

    big update - train script
    train_step & validation_step are in their own functions
    writing_logs in separated func
    temp func direct implementation: make_grid_image, apply_cmap_to tensor
    add visualization of last batch

--- heart_seg_app/train.py
+++ heart_seg_app/train.py
@@ -10,31 +10,22 @@ from heart_seg_app.utils.dataset import (
 from heart_seg_app.utils.metrics import Metrics
 from heart_seg_app.utils.visualization import make_grid_image
 from heart_seg_app.models.unetr import unetr
+from heart_seg_app.utils.metrics import Dice
 
 from monai.data.dataset import Dataset
+from monai.networks.nets import UNETR
 from monai.data import DataLoader, NumpyReader
 from monai.transforms import (
     Compose, LoadImaged, EnsureChannelFirstd, EnsureTyped, Spacingd, 
     RandFlipd, RandAffined, RandGaussianNoised, RandGaussianSmoothd, 
     NormalizeIntensityd, RandAdjustContrastd, Rand3DElasticd,
+    MapTransform,
 )
 from monai.losses import DiceLoss
 from monai.utils import set_determinism, get_seed
 
 import os
 from tqdm import tqdm
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-label_color_map = {
-    "black": [0.0, "background"],
-    "yellow": [1.0, "left ventricle"],
-    "skyblue": [2.0, "right ventricle"],
-    "red": [3.0, "left atrium"],
-    "purple": [4.0, "right atrium"],
-    "blue": [5.0, "myocarium"],
-    "orange": [6.0, "aorta"],
-    "green": [7.0, "the pulmonary artery"],
-}; label_values = [value[0] for value in label_color_map.values()]
 
 hyperparams = {
     "model": "UNETR",
@@ -51,25 +42,195 @@ hyperparams = {
     "seed": None,
 }
 
-def train(model, image_dir, label_dir, dataset_config=None, split_ratios=(0.75, 0.2, 0.05), seed=0, tag="", checkpoint=None, output_dir=None, epochs=3):
-    set_determinism(seed=seed) if seed is not None else get_seed()
-    hyperparams["seed"] = seed
-    hyperparams["epochs"] = epochs
+def collect_data(image_dir : str, label_dir : str) -> list[dict[str, str]]:
+    images = sorted(os.listdir(image_dir))
+    labels = sorted(os.listdir(label_dir))
     
-    data = collect_data_paths(image_dir, label_dir, postfix=".gz.128128128.npy")
-    print(f"Dataset Size: {len(data)}")
-    
-    if dataset_config:
-        dataset = load_config(dataset_config)
-    else:
-        dataset = split_dataset(data, split_ratios=split_ratios, seed=seed)
+    data = []
+    for image, label in zip(images, labels):
+        data.append({"image": os.path.join(image_dir, image),
+                     "label": os.path.join(label_dir, label)})
+        
+    return data
 
-    print("train_size: {}, val_size: {}, test_size: {}".format(len(dataset["train"]), len(dataset["val"]), len(dataset["test"])))
+def get_model(model : str) -> torch.nn.Module:
+    if model == "unetr":
+        hyperparams["model"] = "UNETR"
+        return UNETR(
+            in_channels=1,
+            out_channels=4,
+            img_size=(128, 128, 128),
+            feature_size=16,
+            hidden_size=768,
+            mlp_dim=3072,
+            num_heads=12,
+            proj_type="perceptron",
+            norm_name="instance",
+            res_block=True,
+            dropout_rate=0.0,
+        )
+
+class ToOneHotMetad(MapTransform):
+    def __init__(self, keys : list[str], value_extractor : callable):
+        super().__init__(keys)
+        self.value_extractor = value_extractor
+        
+    def __call__(self, data):
+        d = dict(data); d : dict[str, torch.Tensor]
+        label_values = self.value_extractor(d)
+        for key in self.keys:
+            d[key] = (d[key] == torch.tensor(label_values)[:, None, None, None]).int()
+            
+        return d
+
+def postprocess_outputs(outputs : torch.Tensor, n_classes : int) -> torch.Tensor:
+    segmented = torch.softmax(outputs, dim=1).argmax(dim=1).unsqueeze(dim=1)
+    multilabel_mask = torch.tensor(range(n_classes), device=segmented.device)[None, :, None, None, None]
+    return (segmented == multilabel_mask).float()
+
+def train_step(
+    model : torch.nn.Module,
+    dataloader : DataLoader,
+    optimizer : torch.optim.Optimizer,
+    loss_function : torch.nn.Module,
+    device : torch.device,
+    n_classes : int,
+):
+    model.train()
+    step_loss = 0
+    step_dice = 0
+    step_dice_by_classes = torch.zeros(n_classes, device=device)
+    dice = Dice()
     
+    for batch in tqdm(dataloader):
+        batch : dict[str, torch.Tensor]
+        inputs, targets = batch["image"].to(device), batch["label"].to(device)
+
+        outputs = model(inputs)
+        loss = loss_function(outputs, targets); loss : torch.Tensor
+        loss.backward()
+        step_loss += loss.item()
+        optimizer.step()
+        optimizer.zero_grad()
+        
+        outputs = postprocess_outputs(outputs, 4).int()
+        targets = targets.int()
+        
+        dice_by_classes = dice(outputs, targets)
+        mean_dice = dice.mean().item()
+        step_dice += mean_dice
+        step_dice_by_classes += dice_by_classes
+        
+        
+    step_loss /= len(dataloader)
+    step_dice /= len(dataloader)
+    step_dice_by_classes /= len(dataloader)
+    
+    return step_loss, step_dice, step_dice_by_classes
+
+def validation_step(
+    model : torch.nn.Module,
+    dataloader : DataLoader,
+    loss_function : torch.nn.Module,
+    device : torch.device,
+    n_classes : int,
+):
+    model.eval()
+    step_loss = 0
+    step_dice = 0
+    step_dice_by_classes = torch.zeros(n_classes, device=device)
+    dice = Dice()
+    
+    with torch.no_grad():
+        for batch in tqdm(dataloader):
+            batch : dict[str, torch.Tensor]
+            inputs, targets = batch["image"].to(device), batch["label"].to(device)
+
+            outputs = model(inputs); outputs : torch.Tensor
+            loss = loss_function(outputs, targets); loss : torch.Tensor
+            step_loss += loss.item()
+            
+            outputs = postprocess_outputs(outputs, 4).int()
+            targets = targets.int()
+            
+            dice_by_classes = dice(outputs, targets)
+            mean_dice = dice.mean().item()
+            step_dice += mean_dice
+            step_dice_by_classes += dice_by_classes
+    
+            last_batch = batch
+            last_batch["pred"] = outputs
+        
+    step_loss /= len(dataloader)
+    step_dice /= len(dataloader)
+    step_dice_by_classes /= len(dataloader)
+    
+    return step_loss, step_dice, step_dice_by_classes, last_batch
+
+def write_logs(
+    writer : SummaryWriter,
+    train_loss : float,
+    train_dice : float,
+    train_dice_by_classes : torch.Tensor,
+    val_loss : float,
+    val_dice : float,
+    val_dice_by_classes : torch.Tensor,
+    class_names : list[str],
+    step : int,
+):
+    # train
+    writer.add_scalar("train/loss", train_loss, step)
+    writer.add_scalar("train/mean_dice", train_dice, step)
+    writer.add_scalars("train/mean_dice_by_classes",
+                       dict(zip(class_names, train_dice_by_classes)),
+                       step)
+    
+    # validation
+    writer.add_scalar("val/loss", val_loss, step)
+    writer.add_scalar("val/mean_dice", val_dice, step)
+    writer.add_scalars("val/mean_dice_by_classes",
+                       dict(zip(class_names, val_dice_by_classes)),
+                       step)
+    
+    # compare
+    writer.add_scalars("compare/loss", {"train": train_loss, "val": val_loss}, step)
+    writer.add_scalars("compare/mean_dice", {"train": train_dice, "val": val_dice}, step)
+
+import matplotlib.colors    
+def apply_cmap_to_tensor(tensor : torch.Tensor,
+                         cmap : matplotlib.colors.Colormap,
+                         norm : matplotlib.colors.Normalize = None
+                         ) -> torch.Tensor:
+    tensor = tensor.cpu().numpy()
+    if norm is not None:
+        tensor = norm(tensor)
+    tensor = cmap(tensor) # it converts tensor to numpy rgba image with hwc format, .astype(np.float32)
+    tensor = torch.from_numpy(tensor)
+    tensor = tensor[:,:,:3] # delete alpha channel
+    tensor = tensor.permute(2, 0, 1) # hwc -> chw
+
+    return tensor
+
+from torchvision.utils import make_grid
+import matplotlib.cm
+import matplotlib.pyplot as plt
+from heart_seg_app.utils.visualization import create_custom_cmap
+def make_grid_image(images : list[torch.Tensor],
+                    cmaps : list[matplotlib.colors.Colormap],
+                    norms : list[matplotlib.colors.Normalize],
+                    idx : int):
+    for i, image in enumerate(images):
+        image = torch.Tensor(image).squeeze().cpu()[:,:,idx].T
+        images[i] = apply_cmap_to_tensor(image, cmaps[i], norms[i])
+
+    return make_grid(images)
+
+def get_transforms():
     train_transforms = Compose([
         LoadImaged(keys=["image", "label"], reader=NumpyReader),
         EnsureChannelFirstd(keys=["image"]),
-        ToOneHotd(keys=["label"], label_values=label_values),
+        ToOneHotMetad(keys=["label"],
+                      value_extractor=lambda _: list(range(4))),
         EnsureTyped(keys=["image", "label"]),
         
         # Spacing
@@ -90,124 +251,131 @@ def train(model, image_dir, label_dir, dataset_config=None, split_ratios=(0.75, 
         RandAdjustContrastd(keys=["image"], prob=0.3, gamma=(0.7, 1.5)),
         RandGaussianNoised(keys=["image"], prob=0.15, mean=0, std=0.05),
         RandGaussianSmoothd(keys=["image"], prob=0.1, sigma_x=(0.5, 1.5)),
-
-        # Crop
-        # RandCropByPosNegLabeld(keys=["image", "label"], label_key="label", spatial_size=(128, 128, 128), num_samples=2), 
     ])
+    
     val_transforms = Compose([
         LoadImaged(keys=["image", "label"], reader=NumpyReader),
         EnsureChannelFirstd(keys=["image"]),
-        ToOneHotd(keys=["label"], label_values=label_values),
+        ToOneHotMetad(keys=["label"],
+                      value_extractor=lambda _: list(range(4))),
         EnsureTyped(keys=["image", "label"]),
         Spacingd(keys=["image", "label"], pixdim=(1.0, 1.0, 1.0), mode=("bilinear", "nearest")),
         NormalizeIntensityd(keys=["image"], channel_wise=True),    
     ])
+    
+    return train_transforms, val_transforms
+          
+def train(model : str,
+          image_dir : str,
+          label_dir : str,
+          dataset_config : str = None,
+          split_ratios : tuple[float, float, float] = (0.75, 0.2, 0.05),
+          seed : int = None,
+          tag : str = "",
+          checkpoint : str = None,
+          output_dir : str = None,
+          epochs : int = 3,
+          batch_size : int = 1,
+          lr : float = 1e-4,
+          device : torch.device = "cuda" if torch.cuda.is_available() else "cpu"
+          ) -> None:
+    seed = seed if seed is not None else get_seed()
+    set_determinism(seed=seed)
+    # hyperparams["seed"] = seed
+    # hyperparams["epochs"] = epochs
+
+    data = collect_data(image_dir, label_dir)
+    print(f"Dataset Size: {len(data)}")
+    if dataset_config:
+        dataset = load_config(dataset_config)
+    else:
+        dataset = split_dataset(data, split_ratios=split_ratios, seed=seed)
+    
+    print("train_size: {}, val_size: {}, test_size: {}".format(len(dataset["train"]), len(dataset["val"]), len(dataset["test"])))
+    
+    train_transforms, val_transforms = get_transforms()
 
     train_dataset = Dataset(dataset["train"], transform=train_transforms)
     val_dataset = Dataset(dataset["val"], transform=val_transforms)
     
-    train_dataloader = DataLoader(train_dataset, batch_size=1)
-    val_dataloader = DataLoader(val_dataset, batch_size=1)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
     
     # model
-    if model == "unetr":
-        model = unetr()
-        hyperparams["model"] = "UNETR"
+    model = get_model(model); model : torch.nn.Module
     if checkpoint:
         print("Load Checkpoint:", os.path.basename(checkpoint))
         model.load_state_dict(torch.load(os.path.join(checkpoint), weights_only=True))
     model = model.to(device)
     
-    loss_function = DiceLoss(smooth_nr=0, smooth_dr=1e-5, squared_pred=True, sigmoid=True)
+    loss_function = DiceLoss(smooth_nr=0, smooth_dr=1e-5, squared_pred=True, softmax=True)
     optimizer = torch.optim.AdamW(model.parameters(), **(hyperparams["optimizer"]["params"]))
     
     if output_dir:
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-            print("Save run's path: ", os.path.abspath(output_dir))
-            
-        
         checkpoints_dir = os.path.join(output_dir, "checkpoints")
-        if not os.path.exists(checkpoints_dir):
-            os.makedirs(checkpoints_dir)
+        os.makedirs(output_dir, exist_ok=True)
+        os.makedirs(checkpoints_dir, exist_ok=True)
+        print("Save run's path: ", os.path.abspath(output_dir))
         
         writer = SummaryWriter(os.path.join(output_dir, tag))
     
-        dataset["label_color_map"] = label_color_map
-        print("Save Dataset Config: ", os.path.join(output_dir, tag, "dataset.json"))
-        print("Save Hyperparams Config: ", os.path.join(output_dir, tag, "hyperparams.json"))
         save_config(dataset, os.path.join(output_dir, tag, "dataset.json"))
+        print("Save Dataset Config: ", os.path.join(output_dir, tag, "dataset.json"))
         save_config(hyperparams, os.path.join(output_dir, tag, "hyperparams.json"))
-        
-    # training step
-    best_val_mean_dice = 0
+        print("Save Hyperparams Config: ", os.path.join(output_dir, tag, "hyperparams.json"))
+    
+    best_dice = 0
     for epoch in range(epochs):
-        model.train()
-        train_loss = 0
-        train_mean_dice = 0
-        train_mean_dice_by_classes = torch.zeros(len(label_values), device=device)
-        for batch in tqdm(train_dataloader):
-            inputs, targets = batch["image"].to(device), batch["label"].to(device)
-            outputs = model(inputs)
-            loss = loss_function(outputs, targets)
-            loss.backward()
-            train_loss += loss.item()
-            optimizer.step()
-            optimizer.zero_grad()
-            
-            outputs = label_postprocessing(outputs)
-            targets = targets.int().squeeze(0)
-            
-            metrics = Metrics(outputs, targets)
-            mean_dice = metrics.meanDice().item()
-            dice_by_classes = metrics.Dice()
-            train_mean_dice += mean_dice
-            train_mean_dice_by_classes += dice_by_classes
-            
-        train_loss /= len(train_dataloader)
-        train_mean_dice /= len(train_dataloader)
-        train_mean_dice_by_classes /= len(train_dataloader)
+        # train step
+        train_loss, train_dice, train_dice_by_classes = train_step(
+            model, 
+            train_dataloader, 
+            optimizer,
+            loss_function, 
+            device,
+            4,
+        )
         
         # validation step
-        with torch.no_grad():
-            model.eval()
-            val_loss = 0
-            val_mean_dice = 0
-            val_mean_dice_by_classes = torch.zeros(len(label_values), device=device)
-            for batch in tqdm(val_dataloader):
-                inputs, targets = batch["image"].to(device), batch["label"].to(device)
-                outputs = model(inputs)
-                loss = loss_function(outputs, targets)
-                val_loss += loss.item()
-                
-                outputs = label_postprocessing(outputs)
-                targets = targets.int().squeeze(0)
-                
-                metrics = Metrics(outputs, targets)
-                mean_dice = metrics.meanDice().item()
-                dice_by_classes = metrics.Dice()
-                val_mean_dice += mean_dice
-                val_mean_dice_by_classes += dice_by_classes
-            val_mean_dice /= len(val_dataloader)
-            val_mean_dice_by_classes /= len(val_dataloader)
-            print(f"{epoch+1}/{epochs}: train_loss={train_loss:.5}, train_mean_dice={train_mean_dice:.5}",
-                  "by classes: ", [f"{elem:.5}" for elem in train_mean_dice_by_classes])
-            print(f"{epoch+1}/{epochs}: val_loss={val_loss:.5}, val_mean_dice={val_mean_dice:.5}",
-                  "by classes: ", [f"{elem:.5}" for elem in val_mean_dice_by_classes])
+        val_loss, val_dice, val_dice_by_classes, last_batch = validation_step(
+            model,
+            val_dataloader,
+            loss_function,
+            device,
+            4,
+        )
+        
+        print(f"{epoch+1}/{epochs}: train_loss={train_loss:.5}, train_mean_dice={train_dice:.5}",
+                  "by classes: ", [f"{elem:.5}" for elem in train_dice_by_classes])
+        print(f"{epoch+1}/{epochs}: val_loss={val_loss:.5}, val_mean_dice={val_dice:.5}",
+                  "by classes: ", [f"{elem:.5}" for elem in val_dice_by_classes])
+        
+        if output_dir:
+            if best_dice < val_dice:
+                best_dice = val_dice
+                torch.save(model.state_dict(), os.path.join(output_dir, "checkpoints", f"{tag}.pth"))
+            else:
+                print(f"No improvement in val mean dice, skip saving checkpoint \
+                      best_val_mean_dice={best_dice:.5}")
+            # TODO: do more dynamic
+            class_names = ["background", "left ventricle", "right ventricle", "ascending aorta"]
+            write_logs(writer, train_loss, train_dice, train_dice_by_classes, val_loss, val_dice, val_dice_by_classes, class_names, epoch+1)
             
-            if output_dir:
-                if val_mean_dice > best_val_mean_dice:
-                    best_val_mean_dice = val_mean_dice
-                    torch.save(model.state_dict(), os.path.join(output_dir, "checkpoints", f"{tag}.pth"))
-                else:
-                    print(f"No improvement in val mean dice, skip saving checkpoint\n best_val_mean_dice={best_val_mean_dice:.5}")
-                
-                writer.add_scalar("train_loss", train_loss, epoch+1)
-                writer.add_scalar("train_mean_dice", train_mean_dice, epoch+1)
-                writer.add_scalar("val_loss", val_loss, epoch+1)
-                writer.add_scalar("val_mean_dice", val_mean_dice, epoch+1)
-                class_names = [value[1] for value in label_color_map.values()]
-                writer.add_scalars("train_mean_dice_by_classes", {class_names[i]: train_mean_dice_by_classes[i] for i in range(len(label_color_map))}, epoch+1)
-                writer.add_scalars("val_mean_dice_by_classes", {class_names[i]: val_mean_dice_by_classes[i] for i in range(len(label_color_map))}, epoch+1)
-                img_grid = make_grid_image("tensorboard", inputs, targets, outputs, label_color_map, 50)
-                writer.add_image("validation_grid", img_grid, epoch+1)
+            # visualize last batch
+            image, label, pred = last_batch["image"][0], last_batch["label"][0], last_batch["pred"][0]
+            
+            image = torch.squeeze(image, dim=0)
+            label = torch.argmax(label, dim=0)
+            pred = torch.argmax(pred, dim=0)
+
+            colors = ["black", "yellow", "skyblue", "orange"]
+            label_values = list(range(4))
+            cmap, norm = create_custom_cmap(label_values, colors)
+            
+            img_grid = make_grid_image([image, label, pred],
+                                       [plt.get_cmap("bone"), cmap, cmap],
+                                       [None, norm, norm],
+                                       50)
+            writer.add_image("val/image_grid", img_grid, epoch+1)
+    writer.close()
+    print(f"Training complete. Best validation Dice: {best_dice:.4f}")
